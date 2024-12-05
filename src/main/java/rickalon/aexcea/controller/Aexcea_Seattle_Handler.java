63f347d74ed117a98366d79b971a0f45f44e4418
Ricardo <ricardoaloof@gmail.com>
2024-12-05 19:23:03 +0100

    Endpoints added CRUD

--- src/main/java/rickalon/aexcea/controller/Aexcea_Seattle_Handler.java
+++ src/main/java/rickalon/aexcea/controller/Aexcea_Seattle_Handler.java
@@ -1,24 +1,32 @@
 package rickalon.aexcea.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/seattle")
 public class Aexcea_Seattle_Handler {
+
+    @Value("${api.facegeneration.url}")
+    private String faceGeneratorUrl;
     
     @GetMapping("/users")
-    public void GetUser(){
-        System.out.println("Get user");
+    public ResponseEntity<byte[]> GetUser(RestTemplate restTemplate){
+        byte[] response = restTemplate.getForObject(faceGeneratorUrl, byte[].class);
+        return ResponseEntity.ok().contentType(MediaType.IMAGE_JPEG).body(response);
     }
 
     @PostMapping("/users")
-    public void AddUser(){
-        System.out.println("Add user");
+    public void AddUser(RestTemplate restTemplate){
+        byte[] response = restTemplate.getForObject(faceGeneratorUrl, byte[].class);
     }
 
     @PatchMapping("/users")
