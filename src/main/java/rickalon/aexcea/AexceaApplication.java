0b25dd1a556d4d9ff06fb5c24cee972b6ce3f0df
Ricardo <ricardoaloof@gmail.com>
2024-12-05 13:20:06 +0100

    Base proyect structure

---
+++ src/main/java/rickalon/aexcea/AexceaApplication.java
@@ -0,0 +1,13 @@
+package rickalon.aexcea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AexceaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AexceaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rickalon/aexcea/AexceaApplicationTests.java
@@ -0,0 +1,13 @@
+package rickalon.aexcea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AexceaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
