03cf93d9ab914089b7c553113a558ae718492c92
Ricardo <ricardoaloof@gmail.com>
2024-12-05 14:39:34 +0100

    DB Connection postgres added

--- src/test/java/rickalon/aexcea/AexceaApplicationTests.java
+++ src/test/java/rickalon/aexcea/AexceaApplicationTests.java
@@ -2,12 +2,12 @@ package rickalon.aexcea;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
 @SpringBootTest
 class AexceaApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
 	}
 
 }
